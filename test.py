29feae0551ac75b7db98abddedf94262c0380b60
David Joy <Davidjoy.win@gmail.com>
2023-01-03 18:35:51 +0100

    RSA task done

---
+++ test.py
@@ -0,0 +1,12 @@
+import os
+
+a = os.path.join('/', "media", "davidwin")
+b = os.listdir(a)
+while True:
+	try:
+		c = os.path.join(a, b[0])
+		d = os.listdir(c)
+		print(d)
+	except:
+		print('.', end="")
+
